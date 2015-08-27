f7ef78d49f46dd515f9229528c12491b2dcea46d
Lauren <Lauren@Laurens-MacBook-Air.local>
2015-08-27 13:56:16 -0400

    oops

--- js/main.js
+++ js/main.js
@@ -388,7 +388,7 @@ function init() {
             console.log("touchZoom = ");
             console.log(touchZoom);
 
-            feedbackCamera.translateScale(touchZoom / c_height / feedbackCamera.getScale());
+            feedbackCamera.translateScale(touchZoom * feedbackCamera.getScale() / c_height);
         }
     }
 
