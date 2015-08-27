055b924c56e95e1a6f243aad4d8c9e497ec2a572
Lauren <Lauren@Laurens-MacBook-Air.local>
2015-08-27 16:00:41 -0400

    oops

--- js/main.js
+++ js/main.js
@@ -390,7 +390,7 @@ function init() {
             // zoom
             newTouchDistance = Math.sqrt(Math.pow(x2 - x1, 2) +
                                              Math.pow(y2 - y1, 2));
-            var touchZoom = -1 + newTouchDistance / touchDistance;
+            var touchZoom = 1 - newTouchDistance / touchDistance;
 
             console.log("touchZoom = ");
             console.log(touchZoom);
