f7d124336bc356872a3878c2200671472272b61f
aballl <ball.alex.509@gmail.com>
2017-02-02 20:28:30 -0500

    Start ray tracing at the correct layer

--- js/shaders/RayTracingShader.js
+++ js/shaders/RayTracingShader.js
@@ -16,7 +16,8 @@ RayTracingShader = (function() {
 	return {
 
 		defines: {
-			MAX_DEPTH : maxDepth
+			MAX_DEPTH : maxDepth,
+			MAX_LAYERS : maxDepth + 1
 		},
 
 		uniforms: {
@@ -46,11 +47,10 @@ uniform vec2 resolution;
 // {aspect, distance from eye to sampling plane}
 uniform vec2 projection;
 uniform vec2 portalWidthHeight;
-// uniform vec3 layerColors[MAX_DEPTH];
+uniform sampler2D layerColors;
 
 vec4 getColorFromDepth(int depth) {
-	// return vec4(layerColors[depth], 1);
-	return vec4(vec3(1, 1, 1) * float(depth) / float(MAX_DEPTH), 1);
+	return vec4(vec3(1, 1, 1) * float(depth + 1) / float(MAX_LAYERS), 1);
 }
 
 int getDepth(vec2 st) {
@@ -58,9 +58,26 @@ int getDepth(vec2 st) {
 	return int(floor(color.r * 256.0 + 0.5));
 }
 
-int getIntersectionDepth(vec4 eye, vec2 dstdz) {
+int getIntersectionDepth(vec4 eye, vec2 dstdz, float dir) {
+	int topLayer = int(max(floor(-eye.z), -1.0));
+	if (dir == 0.0 || (dir == 1.0 && topLayer == -1)) {
+		return topLayer;
+	}
+	int increment = -int(dir);
+	int i;
+	int end;
+	if (dir == 1.0) {
+		i = topLayer;
+		end = -1;
+	} else {
+		i = topLayer + 1;
+		end = MAX_DEPTH + 1;
+	}
 	vec2 texCoord;
-	for (int i = 0; i <= MAX_DEPTH; i++) {
+	for (int j = 0; j <= MAX_DEPTH; j++) {
+		if (i == end) {
+			return end;
+		}
 		texCoord = eye.st + dstdz * (-layerSpacing * float(i) - eye.z); 
 		if (texCoord.s < 0.0 
 				|| texCoord.s > 1.0 
@@ -69,8 +86,9 @@ int getIntersectionDepth(vec4 eye, vec2 dstdz) {
 				|| getDepth(texCoord) <= i) {
 			return i;
 		}
+		i += increment;
 	}
-	return MAX_DEPTH + 1;
+	return end;
 }
 
 void main() {
@@ -93,7 +111,7 @@ void main() {
 	
 	vec2 dstdz = vec2(dir.x / dir.z, dir.y / dir.z);
 
-	gl_FragColor = getColorFromDepth(getIntersectionDepth(eye, dstdz));
+	gl_FragColor = getColorFromDepth(getIntersectionDepth(eye, dstdz, sign(dir.z)));
 } 
 
 `};
