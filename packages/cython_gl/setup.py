0a856047518512516990d2a69e7c4920908446bf
Alex Ball <me@alexjball.com>
2021-05-26 18:16:18 -0400

    fix watch and build all pyx files

--- packages/cython_gl/setup.py
+++ packages/cython_gl/setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(
-        [Extension("draw_triangle", ["*.pyx"], libraries=["GL", "glut"])],
+        [Extension("*", ["*.pyx"], libraries=["GL", "glut"])],
         compiler_directives={"language_level": 3},
     ),
     zip_safe=False,
