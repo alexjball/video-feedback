c989c0c24f848b762e12f5f39ef6b46d90da43a4
Alex Ball <me@alexjball.com>
2021-12-07 12:18:58 -0500

    Legend polish
    add faded white background, use LL/UR rather than cardinal directions, color mirror

--- studio/src/legend.tsx
+++ studio/src/legend.tsx
@@ -3,11 +3,43 @@ import styled from "styled-components"
 import { useAppSelector } from "./hooks"
 import { Coords } from "./simulation/model"
 
-type Corner = "NE" | "SE" | "NW" | "SW"
+/**
+ * Renders the simulation model using SVG.
+ *
+ * The legend uses the same frame as the simulation for convenience of
+ * calculation.
+ */
+export function LegendPanel(props: any) {
+  const state = useAppSelector(s => s.simulation)
+  return (
+    <Legend viewBox="-1.5 -1.5 3 3" {...props}>
+      <Destination
+        color="#ff0000"
+        portal={state.portal.coords}
+        borderWidth={state.border.width}
+        mirrorX={state.spacemap.mirrorX}
+        mirrorY={state.spacemap.mirrorY}
+      />
+      <Source
+        color="#00ff00"
+        portal={state.portal.coords}
+        spacemap={state.spacemap.coords}
+        mirrorX={state.spacemap.mirrorX}
+        mirrorY={state.spacemap.mirrorY}
+      />
+    </Legend>
+  )
+}
+
+type Corner = "UR" | "LR" | "UL" | "LL"
+const corners = ["UR", "LR", "UL", "LL"] as Corner[]
 
 const Legend = styled.svg`
-    border: 3px solid black;
-    border-radius: 5px;
+    /* border: 3px solid black; */
+    background-color: rgba(255, 255, 255, 0.3);
+    border-radius: 10px;
+    margin: 20px;
+    box-shadow: 0 0 10px 10px rgba(255, 255, 255, 0.3);
     transform: scaleY(-1);
 
     text {
@@ -24,6 +56,7 @@ const Legend = styled.svg`
     width,
     height
   }) => {
+    const borderOffset = borderWidth > 0 ? 2 * borderWidth : -0.01
     return (
       <>
         <defs>
@@ -33,13 +66,11 @@ const Legend = styled.svg`
           </mask>
         </defs>
         <CenteredRect
-          strokeWidth={2 * borderWidth}
-          stroke={color}
+          fill={color}
           mask="url(#outsideRectangle)"
-          rx={0.01}
-          ry={0.01}
-          width={width}
-          height={height}
+          rx={borderWidth > 0.05 ? 0.1 : 0}
+          width={width + borderOffset}
+          height={height + borderOffset}
         />
       </>
     )
@@ -81,15 +112,24 @@ const Legend = styled.svg`
       height = parentHeight * 0.5,
       halfW = width / 2,
       halfH = height / 2,
-      cx = corner.includes("W") ? -halfW : halfW,
-      cy = corner.includes("S") ? -halfH : halfH
+      lower = corner[0] === "L",
+      left = corner[1] === "L",
+      cx = left ? -halfW : halfW,
+      cy = lower ? -halfH : halfH
 
-    const label = `${mirrorY ? "S" : corner[0]}${mirrorX ? "W" : corner[1]}`,
-      flipX = mirrorX && corner.includes("E"),
-      flipY = mirrorY && corner.includes("N")
+    const label = `${mirrorY ? "L" : corner[0]}${mirrorX ? "L" : corner[1]}`,
+      flipX = mirrorX && !left,
+      flipY = mirrorY && !lower,
+      flip = flipX || flipY
     return (
-      <g display={(flipX || flipY) && hideOnMirror ? "none" : undefined} fillOpacity={0.4}>
-        <rect x={cx - halfW} y={cy - halfH} width={width} height={height} fill={color} />
+      <g display={flip && hideOnMirror ? "none" : undefined} fillOpacity={flip ? 0.4 : 0.4}>
+        <rect
+          x={cx - halfW}
+          y={cy - halfH}
+          width={width}
+          height={height}
+          fill={flip ? "#517fff" : color}
+        />
         <g
           transform={`
             translate(${cx}, ${cy})
@@ -125,7 +165,7 @@ function Destination({
   return (
     <g>
       <Border color={color} borderWidth={borderWidth} width={width} height={height} />
-      {(["NW", "NE", "SE", "SW"] as const).map((c, i) => (
+      {corners.map((c, i) => (
         <Quadrant
           key={i}
           corner={c}
@@ -168,7 +208,7 @@ function Source({
         rotate(${rotationAngle})
         scale(${spacemap.scale.x} ${spacemap.scale.y})
       `}>
-      {(["NW", "NE", "SE", "SW"] as const).map((c, i) => (
+      {corners.map((c, i) => (
         <Quadrant
           key={i}
           corner={c}
@@ -181,33 +221,5 @@ function Source({
         />
       ))}
     </g>
-  )
-}
-
-/**
- * Renders the simulation model using SVG.
- *
- * The legend uses the same frame as the simulation for convenience of
- * calculation.
- */
-export function LegendPanel(props: any) {
-  const state = useAppSelector(s => s.simulation)
-  return (
-    <Legend viewBox="-2 -2 4 4" {...props}>
-      <Destination
-        color="#ff0000"
-        portal={state.portal.coords}
-        borderWidth={state.border.width}
-        mirrorX={state.spacemap.mirrorX}
-        mirrorY={state.spacemap.mirrorY}
-      />
-      <Source
-        color="#00ff00"
-        portal={state.portal.coords}
-        spacemap={state.spacemap.coords}
-        mirrorX={state.spacemap.mirrorX}
-        mirrorY={state.spacemap.mirrorY}
-      />
-    </Legend>
   )
 }
