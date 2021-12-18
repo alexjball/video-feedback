31974ee9d55473b4a98de5214e6e88cd89de9a0b
Alex Ball <me@alexjball.com>
2021-12-18 11:19:32 -0500

    Remove old three.js wrapper
    moved this to canvas and simulation/renderer

--- studio/src/simulation/index.tsx
+++ studio/src/simulation/index.tsx
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic"
-import { BaseProps } from "../three"
+import { BaseProps } from "../canvas"
 import { useInteractions } from "./interactions"
 
 export { reducer } from "./model"
